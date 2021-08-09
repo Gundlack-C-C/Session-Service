116315c65d99a49a61d7daeb3ec1cfde93b05ca9
Thomas Herbelin <herbelin.thomas@gmail.com>
2021-08-09 12:21:34 +0200

    chores: add configuration for deployment

--- app/RabbitMQ/Configuration.py
+++ app/RabbitMQ/Configuration.py
@@ -1,10 +1,12 @@
+import os
+
 EXCHANGE_NAME = "smartsearch"
 ROUTING = {
     'sklearn': {
-        'QUEUE_NAME_INPUT':  "sklearn_queue_input"
+        'QUEUE_NAME_INPUT':  os.getenv("QUEUE_SKLEARN_INPUT")
     },
     'transformers': {
-        'QUEUE_NAME_INPUT':  "transformers_queue_input"
+        'QUEUE_NAME_INPUT':  os.getenv("QUEUE_TRANSFORMERS_INPUT")
     }
 }
 
