04ea517f98610b56aee976cd35e3a64af1b9b845
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-10 13:30:57 +0200

    add input validation to SessonService

--- app/SessionServer.py
+++ app/SessionServer.py
@@ -1,19 +1,32 @@
 import logging
 import uuid
-from werkzeug.exceptions import NotImplemented, ServiceUnavailable
+from werkzeug.exceptions import ServiceUnavailable, BadRequest
 from RabbitMQ.Sender import sendMessage
+from Exception.IncorrectInputException import IncorrectInputException
+
+
+def validateInput(input):
+    try:
+        assert input.get('text', False) != False, '"text" missing!'
+        assert input.get('param', False) != False, '"param" missing!'
+        assert input['param'], '"param" empty!'
+    except Exception as e:
+        raise IncorrectInputException('Missing Field! ' + str(e)) from e
 
 
 class SessionServer():
-    def get_total_active(self):
-        raise NotImplemented("")
-
     def start_session(self, input, mode, _id=None):
+        try: 
+            validateInput(input)
+        except Exception as e:
+            raise BadRequest("Invalid Input! " + str(e)) from e
+
         _id = _id if _id else uuid.uuid4().hex
         try:
             sendMessage({"id": _id, "input": input}, routing_key=mode)
 
-            sendMessage({"id": _id, "state": "NEW", "msg": "New session commited to queue."}, routing_key='status')
+            sendMessage({"id": _id, "state": "NEW",
+                         "msg": "New session commited to queue."}, routing_key='status')
         except Exception as e:
             logging.error(e)
             raise ServiceUnavailable(
