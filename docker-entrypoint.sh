93d301eb00494f7bf34c0840de3c184c4f8b47b1
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-12 15:30:38 +0200

    Change Environment Var ENVIRONMENT to MODE

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 echo "##### Input ######"
-printf "Container Environment: [%s]\n" "$ENVIRONMENT"
+printf "Container Environment: [%s]\n" "$MODE"
 printf "Application name : [%s]\n" "$APPLICATION_NAME"
 printf "Listen Port : [%s]\n" "$PORT"
 printf "Log level : [%s]\n" "$LOG_LEVEL"
 echo "##################"
 echo ""
 
-if [ $ENVIRONMENT = "PROD" ]; then
+if [ $MODE = "PROD" ]; then
     echo "Start production server"
     SCRIPT_NAME=/"$APPLICATION_NAME" gunicorn -w 2 -b 0.0.0.0:"$PORT" --log-level "$LOG_LEVEL" server:app
-elif [ $ENVIRONMENT = "SESSION-STATUS-SERVICE" ]; then
+elif [ $MODE = "SESSION-STATUS-SERVICE" ]; then
     echo "Start Session Status Service"
     python3 ./session-status-service-app.py --production
-elif [ $ENVIRONMENT = "SESSION-SERVICE" ]; then
+elif [ $MODE = "SESSION-SERVICE" ]; then
     echo "Start Session Status Service"
     python3 ./server.py --production
-elif [ $ENVIRONMENT = "DEV" ]; then
+elif [ $MODE = "DEV" ]; then
     echo "Development Mode - Sleep to connect to container"
     /bin/sh -c "while sleep 1000; do :; done"
 else
