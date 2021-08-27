ef390ae56e11ee5cf63beeace3a9cc88c524fe22
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-27 08:57:27 +0200

    add check for complete Environment Variables

--- app/RabbitMQ/Configuration.py
+++ app/RabbitMQ/Configuration.py
@@ -1,4 +1,13 @@
 import os
+import logging
+from Exception.RabbitMQException import RabbitMQException
+
+REQUIRED_VAR = [
+    "RABBITMQ_EXCHANGE_NAME", 
+    "RABBITMQ_SKLEARN_QUEUE_INPUT", 
+    "RABBITMQ_TRANSFORMERS_QUEUE_INPUT", 
+    "RABBITMQ_STATUS_QUEUE_INPUT"
+]
 
 EXCHANGE_NAME = os.getenv("RABBITMQ_EXCHANGE_NAME")
 ROUTING = {
@@ -9,6 +18,18 @@ ROUTING = {
         'QUEUE_NAME':  os.getenv("RABBITMQ_TRANSFORMERS_QUEUE_INPUT")
     },
     'status': {
-        'QUEUE_NAME': os.getenv("RABBITMQ_STATUS_QUEUE")
+        'QUEUE_NAME': os.getenv("RABBITMQ_STATUS_QUEUE_INPUT")
     }
 }
+
+def AssertRabbitmqEnvironmentComplete():
+    complete = True
+    for VAR in REQUIRED_VAR:
+        try:
+            assert os.getenv(VAR) != None, f"Missing Environment Variable: ['{VAR}']"
+        except Exception as e:
+            complete = False
+            logging.error(e)
+
+    if not complete:
+        raise RabbitMQException("Missing Environment Variables!")
